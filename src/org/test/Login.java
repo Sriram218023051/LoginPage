4d4a39ab0ee995fde8e9608023df1de6d57d0ade
Sriram218023051 <Sisriram28@gmil.com>
2021-11-09 15:35:58 +0530

    Password

--- src/org/test/Login.java
+++ src/org/test/Login.java
@@ -2,4 +2,11 @@ package org.test;
 
 public class Login {
 
+	public static void password() {
+		System.out.println("password");
+	}
+
+	public static void main(String[] args) {
+		password();
+	}
 }
